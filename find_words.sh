e0976b32fe79b36d827d6614f05815c596998291
Themelis Kalomoiros <themelis.kalomoiros@teutologic.com>
2021-06-05 20:58:10 +0200

    find_words.sh may find a word if passed as argument or continuesly searches until user gives no word

--- find_words.sh
+++ find_words.sh
@@ -1,10 +1,37 @@
 #!/bin/bash
 
-echo 
-echo 'Enter word:'
-read word
+# Reads a word from stdin.
+function read_word() {
+    read -p 'Enter word: ' word
+    echo "${word}"
+}
 
-echo 
-echo 'Search results:'
-# Flag -i ignores case
-./show_words.py | grep -i "$word"
+# Searches a stored word in application.
+function find_word() {
+    result=$(./show_words.py | grep -i ${1})
+
+    if [[ -z ${result} ]]; then
+        echo "Didn't found entries matching '${1}'."
+    else
+        word_count=$(echo "${result}" | wc -l)
+        echo "Found ${word_count} entries matching '${1}':"
+        # Warning: has to be enclosed in double quotes elsewere it removes any '\n' chars.
+        echo "${result}"
+    fi
+}
+
+# If a word was given as an argument just find that word.
+if [[ -n ${1} ]]; then
+    find_word ${1}
+else
+    while [[ true ]]; do
+        word=$(read_word)
+
+        if [[ -n $word ]]; then
+            find_word ${word}
+        else
+            echo "Done."
+            exit 0
+        fi
+    done
+fi
